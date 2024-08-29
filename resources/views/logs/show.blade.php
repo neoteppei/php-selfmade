0a5a2d8b652bea016c8b9263756b38ec0a522a9c
杉山哲平 <sugiteppei@icloud.com>
2024-08-29 21:52:27 +0900

    Add files via upload

--- resources/views/logs/show.blade.php
+++ resources/views/logs/show.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/ssa.css') }}">
     <title>Log Details</title>
 </head>
 <body>
